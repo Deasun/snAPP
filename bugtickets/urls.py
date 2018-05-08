9d67cfe8f3d2be3e26cbdef91dd2b1da2d0460eb
Deasun OD <des_donn@mailbox.org>
2018-05-08 16:58:31 +0000

    List of reported bugs rendered on to bug_listing.html

--- bugtickets/urls.py
+++ bugtickets/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import report_bug
+from .views import report_bug, get_bug_listing
 
 
 urlpatterns = [
-    url(r'^report_bug/$', report_bug, name="report_bug"),
+    url(r'^$', get_bug_listing, name='get_bug_listing'),
+    url(r'^new/$', report_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/edit/$', report_bug, name='edit_bug')
 ]
--- bugtickets/views.py
+++ bugtickets/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import BugTicket
 from .forms import ReportBugForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 @login_required
 def report_bug(request):
     """
-    Create a new Bug Report ticket
+    Create or edit a Bug Report ticket
     """
     if request.method == 'POST':
         report_form = ReportBugForm(request.POST, request.FILES)
@@ -14,8 +15,19 @@ def report_bug(request):
             submit = report_form.save(commit=False)
             submit.created_by = request.user
             submit.save()
-            return redirect('profile')
+            return redirect('get_bug_listing')
             
     else:
         report_form = ReportBugForm()
     return render(request, 'report_form.html', {'report_form': report_form})
+
+@login_required
+def get_bug_listing(request):
+    """
+    List bugs that were reported prior to 'now'
+    and render them to the 'bug_listing.html'
+    template
+    """
+    
+    bugs = BugTicket.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
+    return render(request, "bug_listing.html", {'bugs': bugs})
