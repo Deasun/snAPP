3ffc30d406b7a9e57b795e13c480294097c46973
Deasun OD <des_donn@mailbox.org>
2018-05-08 12:53:35 +0000

    amended accounts/test_models.py to pass failing test

--- accounts/test_models.py
+++ accounts/test_models.py
@@ -7,4 +7,4 @@ class TestProfileModel(TestCase):
     def test_str_representation_and_one_to_one_relationship(self):
         user = User(username="Test")
         profile = Profile(user=user)
-        self.assertEqual(str(profile), "Profile of user {}".format(user.username))
+        self.assertEqual(str(profile), "{}'s profile".format(user.username))
