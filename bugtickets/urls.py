1fc34e752900dfdddbea18eb40c3137ce121b167
Deasun OD <des_donn@mailbox.org>
2018-05-11 11:34:27 +0000

    views and urls for bug_report and add_comment_to_bug added

--- bugtickets/urls.py
+++ bugtickets/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import report_bug, edit_bug, delete_bug, upvote_bug, get_bug_listing
+from .views import report_bug, edit_bug, delete_bug, upvote_bug, get_bug_listing, add_comment_to_bug, bug_report
 
 
 urlpatterns = [
@@ -9,6 +9,9 @@ urlpatterns = [
     url(r'^edit/(?P<id>\d+)$', edit_bug, name='edit_bug'),
     url(r'^delete/(?P<id>\d+)$', delete_bug, name='delete_bug'),
     url(r'^upvote/(?P<id>\d+)$', upvote_bug, name='upvote_bug'),
+    url(r'^bug/(?P<id>\d+)$', bug_report, name='bug_report'),
+    url(r'^bug/(?P<id>\d+)/comment$', add_comment_to_bug, name='add_comment_to_bug'),
+
 ]
 
 
--- bugtickets/views.py
+++ bugtickets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
-from .models import BugTicket, BugUpvote
-from .forms import ReportBugForm
+from .models import BugTicket, BugUpvote, Comment
+from .forms import ReportBugForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from  django.contrib.messages import success, warning, error
@@ -20,7 +20,6 @@ def report_bug(request, pk=None):
             submit.created_by = request.user
             submit.save()
             return redirect('get_bug_listing')
-            
     else:
         report_form = ReportBugForm()
     return render(request, 'report_form.html', {'report_form': report_form})
@@ -36,7 +35,6 @@ def upvote_bug(request, id=None):
     """Prevent user upvoting own reports"""
     if bug.created_by == request.user:
         messages.error(request, "You cannot upvote your own bug report.")
-    
     else: 
         user = request.user
         vote = bug.upvote(user)
@@ -61,7 +59,6 @@ def edit_bug(request, id):
     """Enable authors only to edit bugs"""
     if submit.created_by != request.user:
         messages.warning(request, "You cannot edit a bug report that you did not generate.")
-        
     else:    
         if request.method == 'POST':
             report_form = ReportBugForm(request.POST, instance=submit)
@@ -70,10 +67,8 @@ def edit_bug(request, id):
                 submit.created_by = request.user
                 submit.save()
                 return redirect('get_bug_listing')
-                    
         else:
             report_form = ReportBugForm(instance=submit)
-    
     return render(request, 'report_form.html', {'report_form': report_form})
 
 
@@ -86,6 +81,26 @@ def delete_bug(request, id=None):
     bugs = BugTicket.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
     return render(request, "bug_listing.html", {'bugs': bugs})
     
+
+@login_required
+def add_comment_to_bug(request, id):
+    post = BugTicket.objects.filter(id=id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('bug_report', id=id)
+    else:
+        form = CommentForm()
+    return render(request, "add_comments_to_bug_form.html", {'form': form})
+
+
+@login_required
+def bug_report(request, id):
+    bugs = BugTicket.objects.filter(id=id)
+    return render(request, "bug_report.html", {'bugs': bugs})
 
 
 @login_required
