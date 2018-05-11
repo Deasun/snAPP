f1d5de8ef4af50ebc2562f15bac734fd3d1483de
Deasun OD <des_donn@mailbox.org>
2018-05-11 11:31:02 +0000

    add_comments_to_bug report model & template created

--- bugtickets/forms.py
+++ bugtickets/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import BugTicket
+from .models import BugTicket, Comment
 
 class ReportBugForm(forms.ModelForm):
     class Meta:
         model = BugTicket
         fields = ('title', 'description')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
