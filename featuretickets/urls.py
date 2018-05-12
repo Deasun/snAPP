192cd528e4126c883138c2249f14a19a8f2773f8
Deasun OD <des_donn@mailbox.org>
2018-05-12 03:56:01 +0000

    routing for featuretickets app

--- featuretickets/urls.py
+++ featuretickets/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import get_feature_listing, request_feature, upvote_bug, bug_report, add_comment_to_bug
+from .views import get_feature_listing, request_feature, upvote_feature, feature_report, add_comment_to_feature
 
 urlpatterns = [
     url(r'^$', get_feature_listing, name='get_feature_listing'),
     url(r'^new/$', request_feature, name='request_feature'),
-    url(r'^upvote/(?P<id>\d+)$', upvote_bug, name='upvote_bug'),
-    url(r'^bug/(?P<id>\d+)$', bug_report, name='bug_report'),
-    url(r'^bug/(?P<id>\d+)/comment$', add_comment_to_bug, name='add_comment_to_bug'),
+    url(r'^upvote/(?P<id>\d+)$', upvote_feature, name='upvote_feature'),
+    url(r'^feature/(?P<id>\d+)$', feature_report, name='feature_report'),
+    url(r'^feature/(?P<id>\d+)/comment$', add_comment_to_feature, name='add_comment_to_feature'),
 ]
