06530a74f123bd938f44321b71f66aca34e426d1
Deasun OD <des_donn@mailbox.org>
2018-05-12 23:38:36 +0000

    created 'cart' app for tracking in session contributions

---
+++ cart/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, reverse
+
+# Create your views here.
+
+def view_cart(request):
+    """A view that renders the contents of cart page"""
+    return render(request, 'cart.html')
+
+
+# Need to change from quantity to specified amount
+def add_to_cart(request, id):
+    """Add a quantity of the specified product to the cart"""
+    quantity=int(request.POST.get('quantity'))
+    
+    cart=request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+# Need to change from quantity to specified amount
+def adjust_cart(request, id):
+    """Adjudt the quantity of the specified product to the specififed amount"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[id] = quantity
+    
+    else: 
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
