5ac3e3a3dbef6ac1b9231979890daa0a41464122
Deasun OD <des_donn@mailbox.org>
2018-05-12 03:02:26 +0000

    urls for feature app- interation with checkout app required

--- featuretickets/urls.py
+++ featuretickets/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import request_feature
-
+from .views import get_feature_listing, request_feature, upvote_bug, bug_report, add_comment_to_bug
 
 urlpatterns = [
-    url(r'^request_feature/$', request_feature, name="request_feature"),
+    url(r'^$', get_feature_listing, name='get_feature_listing'),
+    url(r'^new/$', request_feature, name='request_feature'),
+    url(r'^upvote/(?P<id>\d+)$', upvote_bug, name='upvote_bug'),
+    url(r'^bug/(?P<id>\d+)$', bug_report, name='bug_report'),
+    url(r'^bug/(?P<id>\d+)/comment$', add_comment_to_bug, name='add_comment_to_bug'),
 ]
