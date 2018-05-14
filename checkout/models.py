fb18cc423eb352ca4552e791f65445b84b794398
Deasun OD <des_donn@mailbox.org>
2018-05-14 23:13:49 +0000

    checkout app with model created

---
+++ checkout/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import datetime
+from featuretickets.models import FeatureTicket
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    county_state = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=40, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    date = models.DateField(default=datetime.date.today)
+    
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
+    feature = models.ForeignKey(FeatureTicket, on_delete=models.CASCADE, null=False)
+    quantity = models.IntegerField(blank=False)
+    
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.feature.title, self.feature.contribution)
--- snapp/settings.py
+++ snapp/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'bugtickets',
     'featuretickets',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
