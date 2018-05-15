94d57da2cd08dd01b7289e5884bcf398a8b51ab3
Deasun OD <des_donn@mailbox.org>
2018-05-15 17:12:54 +0000

    amended typo in checkout function

--- checkout/views.py
+++ checkout/views.py
@@ -15,8 +15,7 @@ stripe.api_key = settings.STRIPE_SECRET
 @login_required()
 def checkout(request):
     if request.method=='POST':
-        order_form = OrderForm(regit log
-        quest.POST)
+        order_form = OrderForm(request.POST)
         payment_form = PaymentForm(request.POST)
         
         if order_form.is_valid() and payment_form.is_valid():
