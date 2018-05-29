e16fdc4e79aa0b191fd58b2ff163f0a8b63490a7
Deasun OD <des_donn@mailbox.org>
2018-05-29 23:44:06 +0000

    search description field

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from featuretickets.models import FeatureTicket
 from bugtickets.models import BugTicket
+from bugtickets.charts import chart_data
 
 def feature_search(request):
     features = FeatureTicket.objects.filter(description__icontains=request.GET['q'])
@@ -8,4 +9,4 @@ def feature_search(request):
 
 def bug_search(request):
     bugs = BugTicket.objects.filter(description__icontains=request.GET['q'])
-    return render(request, 'bug_listing.html', {'bugs': bugs})
+    return render(request, 'bug_listing.html', {'bugs': bugs, 'chart_data': chart_data })
