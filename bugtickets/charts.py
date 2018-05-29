146a10885e6d176f4adea414e080336da34b2a2f
Deasun OD <des_donn@mailbox.org>
2018-05-29 21:27:32 +0000

    refactored and moved all chart data code into charts.py

--- bugtickets/charts.py
+++ bugtickets/charts.py
@@ -1,8 +1,9 @@
 import pygal
 from pygal.style import Style
+from .models import BugTicket
 
 """
-Style pygal chart for view
+Style pygal chart for bugticket list
 """
 custom_style = Style(
     font_family='googlefont:Anton',
@@ -22,3 +23,20 @@ custom_style = Style(
     transition='400ms ease-in',
     colors=('#ff01c0', '#1eb13b')
     )
+line_chart = pygal.Line(fill=True, interpolate='cubic', style=custom_style, legend_at_bottom=True, x_label_rotation=-20)
+line_chart.x_labels = 'today', 'last 7 days', 'last 30 days'
+
+"""
+Class methods to filer Active & Complete BugTicket chart data
+"""
+line_chart.add('Started', [
+     int(BugTicket.qs_today_active_bugs()),
+     int(BugTicket.qs_7_day_active_bugs()),
+     int(BugTicket.qs_30_day_active_bugs()),
+         ], dots_size=6)
+line_chart.add('Completed', [
+     int(BugTicket.qs_today_complete_bugs()),
+     int(BugTicket.qs_7_day_complete_bugs()),         
+     int(BugTicket.qs_30_day_complete_bugs()),
+         ], dots_size=6)
+chart_data = line_chart.render_data_uri()    
--- bugtickets/views.py
+++ bugtickets/views.py
@@ -6,13 +6,8 @@ from django.contrib import messages
 from  django.contrib.messages import success, warning, error
 from django.utils import timezone
 import datetime
-import pygal
-from pygal.style import Style
-from .charts import custom_style
+from .charts import chart_data
 
-import json
-from django.core import serializers
-from django.http import HttpResponse
 
 @login_required
 def report_bug(request, pk=None):
@@ -52,7 +47,7 @@ def upvote_bug(request, id=None):
             messages.success(request, "Your upvote has been counted. Thanks")
     
     bugs = BugTicket.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
-    return render(request, "bug_listing.html", {'bugs': bugs})
+    return render(request, "bug_listing.html", {'bugs': bugs, 'chart_data': chart_data })
 
 
 @login_required
@@ -114,30 +109,8 @@ def bug_report(request, pk=id):
 @login_required
 def get_bug_listing(request):
     """
-    List bugs that were reported prior to 'now' and render them to the 'bug_listing.html' template
+    List bugs with most recent on top
     """
     bugs = BugTicket.objects.filter(date_created__lte=timezone.now()).order_by('date_created')
-    
-    """
-    Chart config - importing custom_style from .charts
-    """
-    line_chart = pygal.Line(fill=True, interpolate='cubic', style=custom_style, legend_at_bottom=True, x_label_rotation=-20)
-    line_chart.x_labels = 'today', 'last 7 days', 'last 30 days'
-    
-    """
-    Class methods to query to filer Active & Complete BugTicket chart data
-    """
-    line_chart.add('Started', [
-         int(BugTicket.qs_today_active_bugs()),
-         int(BugTicket.qs_7_day_active_bugs()),
-         int(BugTicket.qs_30_day_active_bugs()),
-             ], dots_size=6)
-    line_chart.add('Completed', [
-         int(BugTicket.qs_today_complete_bugs()),
-         int(BugTicket.qs_7_day_complete_bugs()),         
-         int(BugTicket.qs_30_day_complete_bugs()),
-             ], dots_size=6)
-    chart_data = line_chart.render_data_uri()    
-    
     return render(request, "bug_listing.html", { 'bugs': bugs, 'chart_data': chart_data })
     
