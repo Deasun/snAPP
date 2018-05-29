3fb2f4044b2636ed2b4212c93c7785b1eba5faf0
Deasun OD <des_donn@mailbox.org>
2018-05-29 21:12:15 +0000

    removed queryset and coded in bugtickets/view.py

--- bugtickets/api_views.py
+++ bugtickets/api_views.py
@@ -5,14 +5,9 @@ import datetime
 
 class BugTicketViewSet(generics.ListAPIView):
     """
-    Filter data to GET active bugtickets over the last week
+    Filter data for bugticket data - API view
     """
     queryset = BugTicket.objects.all()
     serializer_class = BugTicketSerializer
-    print(BugTicket.qs_30_day_active_bugs())
-    print(BugTicket.qs_7_day_active_bugs())
-    print(BugTicket.qs_today_active_bugs())
-    print(BugTicket.qs_today_complete_bugs())
-    print(BugTicket.qs_7_day_complete_bugs())
-    print(BugTicket.qs_30_day_complete_bugs())
+
    
