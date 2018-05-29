859025a56f4a089a4ebdf07de64fc839d622471a
Deasun OD <des_donn@mailbox.org>
2018-05-29 21:13:29 +0000

    removed test page and routes for rendering pygal

--- bugtickets/urls.py
+++ bugtickets/urls.py
@@ -1,13 +1,12 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import report_bug, edit_bug, delete_bug, upvote_bug, get_bug_listing, add_comment_to_bug, bug_report, pygalexample
+from .views import report_bug, edit_bug, delete_bug, upvote_bug, get_bug_listing, add_comment_to_bug, bug_report
 from . import api_views
 
 
 urlpatterns = [
     url(r'^$', get_bug_listing, name='get_bug_listing'),
     url(r'^new/$', report_bug, name='report_bug'),
-    url(r'^chart/$', pygalexample, name='pygalexample'),
     url(r'^edit/(?P<id>\d+)$', edit_bug, name='edit_bug'),
     url(r'^delete/(?P<id>\d+)$', delete_bug, name='delete_bug'),
     url(r'^upvote/(?P<id>\d+)$', upvote_bug, name='upvote_bug'),
