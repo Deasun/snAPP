2b7bf89068c476fd475bdf57591eaa5bda02264a
Deasun OD <des_donn@mailbox.org>
2018-05-30 16:46:55 +0000

    half-pie chart added for displaying bug_type reported

--- bugtickets/charts.py
+++ bugtickets/charts.py
@@ -30,13 +30,29 @@ line_chart.x_labels = 'today', 'last 7 days', 'last 30 days'
 Apply class methods to filter current & completed BugTicket chart data
 """
 line_chart.add('Active', [
-     int(BugTicket.qs_today_active_bugs()),
-     int(BugTicket.qs_7_day_active_bugs()),
-     int(BugTicket.qs_30_day_active_bugs()),
+     int(BugTicket.qs_active_bugs(0)),
+     int(BugTicket.qs_active_bugs(7)),
+     int(BugTicket.qs_active_bugs(30)),
          ], dots_size=6)
 line_chart.add('Completed', [
-     int(BugTicket.qs_today_complete_bugs()),
-     int(BugTicket.qs_7_day_complete_bugs()),         
-     int(BugTicket.qs_30_day_complete_bugs()),
+     int(BugTicket.qs_complete_bugs(0)),
+     int(BugTicket.qs_complete_bugs(7)),         
+     int(BugTicket.qs_complete_bugs(30)),
          ], dots_size=6)
 chart_data = line_chart.render_data_uri()    
+
+
+"""
+Half-Pie chart for bug_types
+"""
+pie_chart = pygal.Pie(half_pie=True)
+pie_chart.title = 'Bug Reports Received'
+pie_chart.add('Functional', int(BugTicket.qs_by_bug_type('Functional')))
+pie_chart.add('Communication', int(BugTicket.qs_by_bug_type('Communication')))
+pie_chart.add('Syntax', int(BugTicket.qs_by_bug_type('Syntax')))
+pie_chart.add('Error Notices', int(BugTicket.qs_by_bug_type('Error Notices')))
+pie_chart.add('Calculation Errors', int(BugTicket.qs_by_bug_type('Calculation Errors')))
+pie_chart.add('Flow Problems', int(BugTicket.qs_by_bug_type('Flow Problems')))
+pie_chart_data = pie_chart.render_data_uri()  
+
+
