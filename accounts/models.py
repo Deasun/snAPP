22cee0a13fff120ca26421978da3e2ccf69f398d
Deasun OD <des_donn@mailbox.org>
2018-06-03 17:25:20 +0000

    added alert_date field to profile

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime
 
 
 
@@ -13,6 +14,8 @@ class Profile(models.Model):
 	image = models.ImageField(upload_to='images', default='')
 	trade_union = models.CharField(max_length=100, blank=True)
 	description = models.TextField(null=True, blank=True)
+	alert = models.TextField(null=True, blank=True)
+	alert_date = models.DateField(default=datetime.date.today)
 
 	def __str__(self):
 	    return "{}'s profile".format(self.user)
