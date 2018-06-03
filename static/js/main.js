68466cf83da1cae82331968418e1975b1e11ca05
Deasun OD <des_donn@mailbox.org>
2018-06-03 23:39:53 +0000

    jquery animations included

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-    
+$(document).ready(function(){
 
+// index page animations
+$(".intro-1").hide(0).delay(500).show("fast");
+$(".intro-2").hide(0).delay(2500).show("fast");
+$(".intro-3").hide(0).delay(3500).show("fast");
+$(".intro-4").hide(0).delay(4500).show("fast");
+$(".home-btn").hide(0).delay(6000).show("slow");
 
 });
+
+
+
+
 
