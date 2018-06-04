05403f37ccdb61bfe38446ae1a54a1cb7eb4d01c
Deasun OD <des_donn@mailbox.org>
2018-06-04 02:28:12 +0000

    dropdown menu styled and jquery scroll effect added

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-// index page animations
-$(".intro-1").hide(0).delay(500).show("fast");
-$(".intro-2").hide(0).delay(2500).show("fast");
-$(".intro-3").hide(0).delay(3500).show("fast");
-$(".intro-4").hide(0).delay(4500).show("fast");
-$(".home-btn").hide(0).delay(6000).show("slow");
+    // index page animations
+    $(".intro-1").hide(0).fadeIn(4000);
+    $(".intro-2").hide(0).delay(1000).show("slow");
+    $(".intro-3").hide(0).delay(2000).show("slow");
+    $(".intro-4").hide(0).delay(3000).show("slow");
+    $(".home-btn").hide(0).delay(4000).show("slow");
 
+    // toggle featureticket report details
+    $(".toggle").hide()
+    $(".toggle-button").click(function() {
+        $(".toggle").slideToggle("slow");
+    });
 
-$(".toggle").hide()
-$( ".toggle-button" ).click(function() {
-  $( ".toggle" ).slideToggle( "slow" );
+    $(".toggle2").hide()
+    $(".toggle-button2").click(function() {
+        $(".toggle2").slideToggle("slow");
+    });
+
+    // hover effects
+
+    
+    // profile slide effects
+    
+    
+    // dropdown menu
+    $('.dropdown').on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+    $('.dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+
 });
-
-$(".toggle2").hide()
-$( ".toggle-button2" ).click(function() {
-  $( ".toggle2" ).slideToggle( "slow" );
-});
-
-});
-
-
-
-
-
