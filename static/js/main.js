91d0c47347f64c4325c361ee7756c7fdc563c269
Deasun OD <des_donn@mailbox.org>
2018-06-04 00:25:52 +0000

    jquery toggle added to reports

--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,17 @@ $(".intro-3").hide(0).delay(3500).show("fast");
 $(".intro-4").hide(0).delay(4500).show("fast");
 $(".home-btn").hide(0).delay(6000).show("slow");
 
+
+$(".toggle").hide()
+$( ".toggle-button" ).click(function() {
+  $( ".toggle" ).slideToggle( "slow" );
+});
+
+$(".toggle2").hide()
+$( ".toggle-button2" ).click(function() {
+  $( ".toggle2" ).slideToggle( "slow" );
+});
+
 });
 
 
