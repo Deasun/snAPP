bc273d8243d16e7bf00d3b9c0827680e02fbb272
Deasun OD <des_donn@mailbox.org>
2018-06-05 03:15:38 +0000

    alert added to profile edit form

--- accounts/forms.py
+++ accounts/forms.py
@@ -59,4 +59,4 @@ class UserEditForm(forms.ModelForm):
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ('user', 'alert')
+        exclude = ('user',)
