c0fb392981d8c7469ed03aaf5e9b924d9c024218
Deasun OD <des_donn@mailbox.org>
2018-06-11 02:24:27 +0000

    add jquery styling to icons, logo and tabs

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
     /*index page animations*/
-    $(".intro-1").hide(0).fadeIn(4000);
-    $(".intro-2").hide(0).delay(1000).show("slow");
-    $(".intro-3").hide(0).delay(2000).show("slow");
-    $(".intro-4").hide(0).delay(3000).show("slow");
-    $(".home-btn").hide(0).delay(4000).show("slow");
+    $(".intro-1").hide(0).fadeIn(6000);
+    $(".intro-2").hide(0).delay(1000).show(0);
+    $(".intro-3").hide(0).delay(2000).show(0);
+    $(".intro-4").hide(0).delay(3000).show(0);
+    $(".home-btn").hide(0).delay(4000).show(0);
 
     /*toggle featureticket report details*/
     $(".toggle").hide()
@@ -18,10 +18,10 @@ $(document).ready(function() {
     });
 
     /*profile tabs*/
-    $(".nav-tabs a").click(function(){
-    $(this).tab('show');
+    $(".nav-tabs a").click(function() {
+        $(this).tab('show');
     });
-    
+
     /*dropdown menu*/
     $('.dropdown').on('show.bs.dropdown', function() {
         $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
@@ -32,4 +32,18 @@ $(document).ready(function() {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
 
+    /*Hover over tabs and icons*/
+    $('.tab-pane, i, .stick-head')
+        .mouseenter(function() {
+            $(this).css({ "opacity": ".7" })
+        })
+        .mouseleave(function() {
+            $(this).css({ "opacity": "1" })
+        });
+
+    /*Reduce size/pinch on click*/
+    $('i').click(function() {
+        $(this).animate({opacity: "1"}, 500)
+    })
 });
+
