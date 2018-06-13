4a1be3202075e0f617f5a20985b570a257bba2c0
Deasun OD <des_donn@mailbox.org>
2018-06-13 15:53:01 +0000

    handled empty queryset search result with django message

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.contrib import messages
 from featuretickets.models import FeatureTicket
 from featuretickets.charts import feature_chart_data, feature_pie_chart_data
 from bugtickets.models import BugTicket
@@ -9,11 +10,26 @@ from bugtickets.charts import chart_data, pie_chart_data, bar_chart_data
 
 def feature_search(request):
     features = FeatureTicket.objects.filter(feature_type__icontains=request.GET['q'])
-    return render(request, 'feature_listing.html', {'features': features, 'feature_chart_data': feature_chart_data, 'feature_pie_chart_data': feature_pie_chart_data})
+    
+    # Handle empty query set with django message 
+    if not bugs:
+        messages.success(request, "Your search returned no results. Please try again.")
+    
+    # Pass data to render charts
+    return render(request, 'feature_listing.html', {
+                'features': features, 
+                'feature_chart_data': feature_chart_data, 
+                'feature_pie_chart_data': feature_pie_chart_data
+                })
 
 def bug_search(request):
     bugs = BugTicket.objects.filter(description__icontains=request.GET['q'])
-    bug_votes = BugTicket.objects.all().order_by('-votes')[:3]
+    
+    # Handle empty query set with django message 
+    if not bugs:
+        messages.success(request, "Your search returned no results. Please try again.")
+    
+    # Pass data to render charts
     return render(request, 'bug_listing.html', {
                 'bugs': bugs, 
                 'chart_data': chart_data, 
