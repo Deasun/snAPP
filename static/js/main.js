20446ddaf918bd77069a06fee41db5ee879d9e00
Deasun OD <des_donn@mailbox.org>
2018-06-14 22:29:12 +0000

    intro animations re-timed and content amended to reflect alert system

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     /*index page animations*/
-    $(".intro-1").hide(0).fadeIn(6000);
-    $(".intro-2").hide(0).delay(1000).show(0);
-    $(".intro-3").hide(0).delay(2000).show(0);
-    $(".intro-4").hide(0).delay(3000).show(0);
-    $(".intro-5").hide(0).show(4000);
-    $(".home-btn-1").hide(0).delay(5000).show(0);
+    
+    $(".intro-2").hide(0).delay(2000).show(0);
+    $(".intro-3").hide(0).delay(4000).show(0);
+    $(".intro-4").hide(0).delay(6000).show(0);
+    $(".intro-1").hide(0).fadeIn(8000);
+    $(".home-btn-1").hide(0).delay(8000).show(0);
 
     /*toggle featureticket report details*/
     $(".toggle").hide()
