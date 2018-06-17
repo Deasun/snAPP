2f27b44f18c563b64c8ad47a67b7a65fdfe0d160
Deasun OD <des_donn@mailbox.org>
2018-06-17 17:34:08 +0000

    cart.views testing 91%

--- cart/test_views.py
+++ cart/test_views.py
@@ -5,6 +5,7 @@ from django.contrib.messages import get_messages
 from django.urls import reverse
 from django.contrib.auth.models import User
 from accounts.models import Profile
+from featuretickets.models import FeatureTicket
 
 
 
@@ -13,30 +14,27 @@ from accounts.models import Profile
 Test Cart
 """
 class TestCart(TestCase):
-    """
-    Simulate login object for accessing bugtickets
-    """   
+    """Simulate login object for accessing bugtickets"""   
     def setUp(self):
         self.user = User.objects.create_user('Tested', 'tested@mail.com', 'q0w9e8r7t7')
         self.user.save()
         login = self.client.login(username='Tested', password='q0w9e8r7t7')
     
-    """
-    Test getting cart page
-    """
+    """Test getting cart page"""
     def test_view_cart(self):    
         response = self.client.get("/cart/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "cart.html") 
     
-    """
-    Test adding items to cart
-    """
+    """Test adding items to cart"""
     def test_add_to_cart(self):
-        pass
+        ticket = FeatureTicket.objects.create(created_by_id=1, title='TestTitle', feature_type='Design', description='This is a test') 
+        response = self.client.post('/cart/add/1', {'quantity': '2'})
+        self.assertEqual(response.status_code, 302)
+        
     
-    """
-    Test adjust amount of items in cart
-    """
+    """Test adjust amount of items in cart"""
     def test_adjust_cart(self):
-        pass
+        ticket = FeatureTicket.objects.create(created_by_id=1, title='TestTitle', feature_type='Design', description='This is a test') 
+        response = self.client.post('/cart/add/1', {'quantity': '2'})
+        response = self.client.post('/cart/adjust/1', {'quantity': '3'})
