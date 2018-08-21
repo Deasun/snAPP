febfce4ccb8c36125b1f949d14a362064556365d
Deasun OD <des_donn@mailbox.org>
2018-08-21 17:57:51 +0000

    variables changed for heroku deployment

--- snapp/settings.py
+++ snapp/settings.py
@@ -161,6 +161,8 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
+##
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
@@ -174,6 +176,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ###########TO BE UNCOMMENTED PRIOR TO DEPLOYMENT#########
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+##
 
 # ###########TO BE COMMENTED OUT PRIOR TO DEPLOYMENT#########
 # MEDIA_URL = '/media/'
